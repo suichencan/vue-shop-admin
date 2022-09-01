82d0d3a5844dc953178329986cfb10c43f991ae5
suichencan <suichencan@live.com>
2022-09-01 17:15:09 +0800

    项目框架搭建及404页面配置

---
+++ src/pages/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-result
+    icon="error"
+    title="404"
+    sub-title="您请求的页面不存在"
+  >
+    <template #extra>
+      <el-button type="primary" @click="$router.push('/')">返回首页</el-button>
+    </template>
+  </el-result>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
